a8eb1796f69b871ece5b321dec12b3665e922bd0
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2024-01-15 01:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+Object.prototype.getKeys = function(){
+	let keys = [];
+	for(let i in this){
+		if(this.hasOwnProperty(i)){
+			keys.push(i);
+		}
+	}
+	return keys;
+}
+let student = {
+	name : "xyz",
+}
